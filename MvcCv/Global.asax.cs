233f31159d5f3ee6fa5cf31b8fa8c325d91fbaf1
MUSTAFA YENAL <202803018@ogr.cbu.edu.tr>
2021-10-14 12:56:20 +0300

    Add project files.

---
+++ MvcCv/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace MvcCv
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalFilters.Filters.Add(new AuthorizeAttribute());
+            //tek tek Authorize yazmak yerine bu işlemi yaparak bütün controllara uygulandı (eişim iznimiz yok yani:))
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
